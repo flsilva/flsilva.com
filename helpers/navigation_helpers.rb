0920abd89bf3ec53c97d29fb9b38fef4a68f9948
Flavio Silva <flavio@flsilva.com>
2012-10-23 17:48:58 -0200

    selected navigation items changed to work with regular expressions through navigation_helpers.rb

--- helpers/navigation_helpers.rb
+++ helpers/navigation_helpers.rb
@@ -1,5 +1,7 @@
 module NavigationHelpers
-  def get_active_nav_class(page)
-    (current_resource.url == page) ? "active" : ""
+  def get_active_nav_class(page_regex)
+    #(current_resource.url == page) ? "active" : ""
+    #(Regexp.new(page.downcase).match(current_resource.url.to_s.downcase) != nil) ? "active" : ""
+    (page_regex.match(current_resource.url.to_s.downcase) != nil) ? "active" : ""
   end
 end
