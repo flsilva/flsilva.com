b0ea65b2d11c446fed40593217395209ed56ed8e
Flavio Silva <flavio@flsilva.com>
2012-11-08 14:37:14 -0200

    portfolio

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,1 +1,19 @@
+$(document).ready(function () {
+  configPortfolioSlideshow();
 
+  $('.menu-button').click(function (event) {
+    event.preventDefault();
+
+    $('.main-nav').toggleClass('active');
+  });
+});
+
+function configPortfolioSlideshow()
+{
+  var config = {advanceSpeed:7000, directionalNav:false}
+
+  $("#slide-bienal2012").orbit(config);
+  $("#slide-conspiracao").orbit(config);
+  $("#slide-pepsi").orbit(config);
+  $("#slide-ultraeco").orbit(config);
+}
