2e868c4581e97a99b20644819dc52f03b6a3d0e2
Flavio Silva <flsilva7@gmail.com>
2013-01-21 02:47:53 -0200

    added git workflow post. added warning-icon. added rainbow syntax highlighter. small CSS modifications.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,5 @@
 //= require vendor/jquery-1.8.2.min
 //= require vendor/modernizr-2.6.2.min.js
+//= require vendor/rainbow-custom.min.js
 //= require foundation/jquery.foundation.orbit.js
 //= require_tree .
