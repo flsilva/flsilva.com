cc5b5479aca3f41e026aaa837f9495d6f911fae3
Flavio Silva <flsilva7@gmail.com>
2022-12-02 16:04:44 -0300

    add a temporary placeholder logo.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,20 @@
 import * as React from 'react'
-import { logo } from './Logo.module.css'
+import { StaticImage } from 'gatsby-plugin-image'
+
+import { logo, logoContainer } from './Logo.module.css'
 
 export const Logo: React.FC = () => {
   return (
-    <h1 className={logo}><a href="/">flsilva.com</a></h1>
+    <div className={logoContainer}>
+      <div>
+        <StaticImage
+          alt="flsilva.com image logo"
+          src="./logo-shape.png"
+          width={30}
+          height={26}
+        />
+      </div>
+      <h1 className={logo}><a href="/">flsilva.com</a></h1>
+    </div>
   )
 }
