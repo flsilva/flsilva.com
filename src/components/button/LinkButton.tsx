c99a17e2039b19e923edf1b0a21ad3f80dc4f67e
Flavio Silva <flsilva7@gmail.com>
2023-02-15 09:55:45 -0300

    <LinkButton>: delete TODO comment that should have been deleted (feature was implemented).

--- src/components/button/LinkButton.tsx
+++ src/components/button/LinkButton.tsx
@@ -5,7 +5,7 @@ import { ChildrenProps } from '../ChildrenProps'
 import { linkButton } from './LinkButton.module.css'
 
 interface LinkButtonProps extends ChildrenProps {
-  alignSelf?: 'flex-start' | 'flex-end'; // TODO: implememnt support to this feature or think something else like a <FlexStart /> and <FlexEnd /> components (not sure if it would work as intended here, i.e., align button to start or end of screen)
+  alignSelf?: 'flex-start' | 'flex-end';
   external?: boolean;
   target?: string;
   text: string;
