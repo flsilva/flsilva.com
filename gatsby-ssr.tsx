44bfef3620289d21a2a3ca9f895113098a3c1930
Flavio Silva <flsilva7@gmail.com>
2023-04-04 14:54:24 -0300

    performance: add 'defer' to JS scripts on gatsby-ssr.tsx.

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 
 export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
   const headComponents = getHeadComponents();
-  headComponents.push(<script src="/theming.js" key="theming-js" />);
-  headComponents.push(<script src="/main-nav-menu.js" key="main-nav-menu-js" />);
-  headComponents.push(<script src="/home.js" key="home-js" />);
-  headComponents.push(<script src="/portfolio-project.js" key="portfolio-project-js" />);
+  headComponents.push(<script src="/theming.js" key="theming-js" defer />);
+  headComponents.push(<script src="/main-nav-menu.js" key="main-nav-menu-js" defer />);
+  headComponents.push(<script src="/home.js" key="home-js" defer />);
+  headComponents.push(<script src="/portfolio-project.js" key="portfolio-project-js" defer />);
 
   replaceHeadComponents(headComponents);
 };
