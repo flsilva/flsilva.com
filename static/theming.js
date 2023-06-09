2977a24bd7cbcb90a5f651a23b69cc11377bd78a
Flavio Silva <flsilva7@gmail.com>
2023-06-09 18:26:43 -0300

    theming.js: get 'crossOrigin = 'anonymous';' lines back to fix fetch error.

--- static/theming.js
+++ static/theming.js
@@ -29,12 +29,14 @@ const setupExternalCssFiles = () => {
   githubLightSyntaxHighlighterLinkStyle.href = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/github.min.css';
   githubLightSyntaxHighlighterLinkStyle.integrity = 'sha512-0aPQyyeZrWj9sCA46UlmWgKOP0mUipLQ6OZXu8l4IcAmD2u31EPEy9VcIMvl7SoAaKe8bLXZhYoMaE/in+gcgA==';
   githubLightSyntaxHighlighterLinkStyle.referrerPolicy = 'no-referrer';
+  githubLightSyntaxHighlighterLinkStyle.crossorigin="anonymous";
 
   githubDarkSyntaxHighlighterLinkStyle = document.createElement('link');
   githubDarkSyntaxHighlighterLinkStyle.rel = 'stylesheet';
   githubDarkSyntaxHighlighterLinkStyle.href = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/github-dark.min.css';
   githubDarkSyntaxHighlighterLinkStyle.integrity = 'sha512-rO+olRTkcf304DQBxSWxln8JXCzTHlKnIdnMUwYvQa9/Jd4cQaNkItIUj6Z4nvW1dqK0SKXLbn9h4KwZTNtAyw==';
   githubDarkSyntaxHighlighterLinkStyle.referrerPolicy = 'no-referrer';
+  githubDarkSyntaxHighlighterLinkStyle.crossorigin="anonymous";
 }
 
 const changeTheme = (colorTheme) => {
