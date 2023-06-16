1570f345fa75e9dd540ce36901c2146131da4480
Flavio Silva <flsilva7@gmail.com>
2023-06-16 12:43:19 -0300

    blog tag pages: add new pages.

---
+++ src/pages/blog/ohmyzsh/index.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import type { HeadFC, PageProps } from 'gatsby';
+import { graphql } from 'gatsby';
+
+import { SEO } from '../../../components/SEO';
+import { PostsByTag } from '../../../components/blog/PostsByTag';
+
+const OhMyZshPosts: React.FC<PageProps<Queries.PortfolioProjectsQuery>> = ({ data }) => (
+  <PostsByTag
+    nodes={data.allMdx.nodes.filter(
+      (node) => node.frontmatter.tags && node.frontmatter.tags.indexOf('ohmyzsh') !== -1,
+    )}
+  />
+);
+
+export const query = graphql`
+  query BlogPosts {
+    allMdx(
+      sort: { frontmatter: { date: DESC } }
+      filter: { frontmatter: { contentType: { eq: "blog-post" } } }
+    ) {
+      nodes {
+        id
+        frontmatter {
+          date
+          description
+          heroImage {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+          heroImageCreditText
+          heroImageCreditUrl
+          slug
+          tags
+          title
+        }
+      }
+    }
+  }
+`;
+
+export default OhMyZshPosts;
+
+export const Head: HeadFC = () => (
+  <SEO
+    description="Check out my articles about Oh My Zsh."
+    pagePath="/blog/ohmyzsh"
+    title="Oh My Zsh articles by Flavio Silva: Full-Stack Software Engineer"
+  />
+);
