e0b62f27636a78ad551872c31a00c650cbbd1399
Flavio Silva <flsilva7@gmail.com>
2023-06-16 11:52:22 -0300

    blog post: add /blog/how-to-install-use-antigen-oh-my-zsh-macos/.

---
+++ src/components/boxes/AITools.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export const AITools: React.FC = () => (
+  <p>
+    I used AI tools to assist me in writing this content, but only to research information faster
+    and express my ideas more clearly. It is not copy and paste.
+  </p>
+);
