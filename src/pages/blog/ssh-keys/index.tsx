f1e8a9406dd54a18cf7f1d274e2136797cd9e15a
Flavio Silva <flsilva7@gmail.com>
2023-06-23 15:02:11 -0300

    blog post: add /blog/how-to-generate-use-ssh-keys-macos/.

---
+++ src/pages/blog/ssh-keys/index.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import type { HeadFC, PageProps } from 'gatsby';
+import { graphql } from 'gatsby';
+
+import { SEO } from '../../../components/SEO';
+import { PostsByTag } from '../../../components/blog/PostsByTag';
+
+const SSHKeysPosts: React.FC<PageProps<Queries.PortfolioProjectsQuery>> = ({ data }) => (
+  <PostsByTag
+    nodes={data.allMdx.nodes.filter(
+      (node) => node.frontmatter.tags && node.frontmatter.tags.indexOf('ssh-keys') !== -1,
+    )}
+  />
+);
+
+export const query = graphql`
+  query BlogPosts {
+    allMdx(
+      sort: { frontmatter: { date: DESC } }
+      filter: { frontmatter: { contentType: { eq: "blog-post" } } }
+    ) {
+      nodes {
+        id
+        frontmatter {
+          date
+          description
+          heroImage {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+          heroImageCreditText
+          heroImageCreditUrl
+          slug
+          tags
+          title
+        }
+      }
+    }
+  }
+`;
+
+export default SSHKeysPosts;
+
+export const Head: HeadFC = () => (
+  <SEO
+    description="Check out my articles about SSH Keys."
+    pagePath="/blog/ssh-keys"
+    title="SSH Keys articles by Flavio Silva: Full-Stack Software Engineer"
+  />
+);
