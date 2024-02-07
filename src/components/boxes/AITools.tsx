709f4536d501306b9bf47f0c441d3f1534ef054f
Flavio Silva <flsilva7@gmail.com>
2024-02-07 20:45:55 -0300

    minor copy change to <AITools>

--- src/components/boxes/AITools.tsx
+++ src/components/boxes/AITools.tsx
@@ -5,7 +5,7 @@ export const AITools: React.FC = () => (
   <InfoBox>
     <p>
       I used AI tools to assist me in writing this content, but only to research information faster
-      and express my ideas more clearly. It is not copy and paste.
+      and express my ideas clearly. It&#39;s not copy and paste.
     </p>
   </InfoBox>
 );
