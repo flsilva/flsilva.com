390118962c9c385dd66949a5491a4e661406eeaf
Flavio Silva <flsilva7@gmail.com>
2025-05-06 15:16:40 -0300

    WIP: relational database design series of articles

--- src/features/shared/ui/code/InlineCode.tsx
+++ src/features/shared/ui/code/InlineCode.tsx
@@ -4,7 +4,7 @@ export async function InlineCode({ codeblock }: { codeblock: RawCode }) {
   const highlighted = await highlight(codeblock, "github-from-css");
   return (
     <Inline
-      className="font-geist-mono text-sm rounded-sm py-0.5 px-1 wrap-anywhere group-[.h]:[font-size:inherit]"
+      className="font-geist-mono text-sm font-normal rounded-sm py-0.5 px-1 wrap-anywhere group-[.h]:[font-size:inherit]"
       code={highlighted}
       style={highlighted.style}
     />
