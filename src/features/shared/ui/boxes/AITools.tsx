4c30e06e0b0db2d71c31471cd9a9812aeb777c99
Flavio Silva <flsilva7@gmail.com>
2025-05-09 22:08:05 -0300

    update gen AI tools usage copy.

--- src/features/shared/ui/boxes/AITools.tsx
+++ src/features/shared/ui/boxes/AITools.tsx
@@ -4,9 +4,10 @@ import { P } from "../typography/Typography";
 export const AITools: React.FC = () => (
   <InfoBox>
     <P>
-      I incorporated generative AI tools into my workflow, and I love them. But
-      I use them carefully to brainstorm, research information faster, and
-      express myself clearly. It&#39;s not copy/paste in any way.
+      I&#39;ve integrated generative AI tools into my workflow and find them
+      invaluable. I use them mindfully for brainstorming, speeding up research,
+      and communicating more clearly. It&#39;s not about copy-pasting; it&#39;s
+      a true collaboration.
     </P>
   </InfoBox>
 );
