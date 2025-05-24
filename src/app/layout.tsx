91a8beb7f8ea0f4036ba6ca0e11591bc14441a51
Flavio Silva <flsilva7@gmail.com>
2025-05-24 18:56:02 -0300

    add @vercel/analytics/next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import { GoogleTagManager } from "@next/third-parties/google";
 
 import "./globals.css";
@@ -50,6 +51,7 @@ export default function RootLayout({
           </div>
         </div>
         <MainFooter />
+        <Analytics />
       </body>
     </html>
   );
